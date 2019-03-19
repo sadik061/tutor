dfa036e4ab9cc3e0e3eae0fe4f303b5dd27ff05f
sadik061 <sadik061@gmail.com>
2019-03-19 09:34:12 +0600

    Main template

---
+++ Template/header.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="Dashboard">
+    <meta name="keyword" content="Teacher, Student. Tution">
+    <title>Tutor.com</title>
+
+    <!-- Favicons -->
+    <link href="../static/img/favicon.png" rel="icon">
+    <link href="../static/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+    <!-- Bootstrap core CSS -->
+    <link href="../static/lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <!--external css-->
+    <link href="../static/lib/font-awesome/css/font-awesome.css" rel="stylesheet" />
+    <link rel="stylesheet" type="text/css" href="../static/css/zabuto_calendar.css">
+    <link rel="stylesheet" type="text/css" href="../static/lib/gritter/css/jquery.gritter.css" />
+    <!-- Custom styles for this template -->
+    <link href="../static/css/style.css" rel="stylesheet">
+    <link href="../static/css/style-responsive.css" rel="stylesheet">
+    <script src="../static/lib/chart-master/Chart.js"></script>
+    <link href="../static/css/custom.css" rel="stylesheet">
+
+    <!-- =======================================================
+      Template Name: Dashio
+      Template URL: https://templatemag.com/dashio-bootstrap-admin-template/
+      Author: TemplateMag.com
+      License: https://templatemag.com/license/
+    ======================================================= -->
+</head>
---
+++ routes/index.php
@@ -0,0 +1,4 @@
+<?php include '../Template/header.php'?>
+<body>
+<?php include '../Template/navbar.php'?>
+<?php include '../Template/footer.php'?>
